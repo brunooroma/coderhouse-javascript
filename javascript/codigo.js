0b696f17df771e521aacb679b7bfb0615cbcefa6
bruno.roma <brunoo.roma@gmail.com>
2022-05-19 23:23:19 -0300

    primer commit - probando prompt y alert

---
+++ javascript/codigo.js
@@ -0,0 +1,9 @@
+let nombre = prompt('Ingrese su nombre')
+let apellido = prompt('Ingrese su apellido')
+let edad = prompt('Ingrese su edad')
+let pais = prompt('Ingrese el pais donde vive')
+
+let descripcion = "Me llamo " + nombre + " " + apellido + " tengo " + edad + " años" + " y vivo en " + pais
+
+alert(descripcion)
+console.log(descripcion)
