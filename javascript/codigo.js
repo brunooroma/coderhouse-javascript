28d7ee360272590db7c81621e1aef37ce776c972
bruno.roma <brunoo.roma@gmail.com>
2022-05-29 21:07:09 -0300

    tabla de multiplicar con bucle for, para entrega

--- javascript/codigo.js
+++ javascript/codigo.js
@@ -1,8 +1,18 @@
-let nombre = prompt('Ingrese su nombre')
-let apellido = prompt('Ingrese su apellido')
-let edad = prompt('Ingrese su edad')
-let pais = prompt('Ingrese el pais donde vive')
+/* let nombre = prompt('Ingrese su nombre');
+let apellido = prompt('Ingrese su apellido');
+let edad = prompt('Ingrese su edad');
+let pais = prompt('Ingrese el pais donde vive');
 
-let descripcion = `Me llamo ${nombre} ${apellido} tengo ${edad} años y vivo en ${pais}`
+let descripcion = `Me llamo ${nombre} ${apellido} tengo ${edad} años y vivo en ${pais}`;
 
-document.write(descripcion)
+document.write(descripcion);
+ */
+
+for (let i = 1; i <= 12; i++) {
+    document.write('La tabla del ',i,' es: <br>');
+    for (let y = 1; y <= 12; y++) {
+        document.write(i*y + ', ');
+    }
+    document.write('<br>');
+    document.write('<br>');
+}
