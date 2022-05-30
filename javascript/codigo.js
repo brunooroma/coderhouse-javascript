a489163b7f47b619f85bd243dc7255455e9f1124
bruno.roma <brunoo.roma@gmail.com>
2022-05-29 21:15:41 -0300

    agregue condicional para entrega

--- javascript/codigo.js
+++ javascript/codigo.js
@@ -1,12 +1,21 @@
-/* let nombre = prompt('Ingrese su nombre');
+let nombre = prompt('Ingrese su nombre');
 let apellido = prompt('Ingrese su apellido');
 let edad = prompt('Ingrese su edad');
 let pais = prompt('Ingrese el pais donde vive');
+ 
+let descripcion;
 
-let descripcion = `Me llamo ${nombre} ${apellido} tengo ${edad} años y vivo en ${pais}`;
+// USO DE CONDICIONAL
 
-document.write(descripcion);
- */
+if (edad < 18) {
+    descripcion = `Tu nombre es ${nombre} ${apellido} tenes ${edad} años y vivis en ${pais}, pero sos menor edad`;
+    alert(descripcion);
+} else {
+    descripcion = `Tu nombre es ${nombre} ${apellido} tenes ${edad} años y vivis en ${pais}`;
+    alert(descripcion);
+}
+
+//BUCLE FOR - TABLA MULTIPLICAR
 
 for (let i = 1; i <= 12; i++) {
     document.write('La tabla del ',i,' es: <br>');
